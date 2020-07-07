0824b811d4b2451c7f5960558d120693bf5a442f
横溝 翔貴 <shokimac@Shokis-Mac.local>
2020-07-08 06:50:11 +0900

    Search_action_Temporary

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
     resources :post_comments, only: [:create, :destroy]
   end
   resources :relationships, only: [:create, :destroy]
+  
+  get 'search' => 'search#search'
 
   root 'home#top'
   get 'home/about'
